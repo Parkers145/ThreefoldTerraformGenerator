d3e9e13e030678f70d0f0c9bfd8f088869f1a718
Parkers145 <44621168+Parkers145@users.noreply.github.com>
2023-01-07 17:42:41 -0600

    V1.2
    Refinements to disk addition process 
    added out variables prompts for each 
    -fixed output block over write 
    -fixed duplicate output variable 
    -fixed invalid subnet with default value 
    
    now deploys working main.tf files for use with env.tfvars files

--- scripts/ThreefoldTerraformGenerator.sh
+++ scripts/ThreefoldTerraformGenerator.sh
@@ -24,9 +24,9 @@ generate_grid_network_blocks() {
 	if [[ -z $NetDesc ]]; then 
 	NetDesc="MyNetwork1"
 	fi
-    read -p "Enter the Private Subnet Ip Range of your Network[10.0.0.0/24]" IP_RANGE
+    read -p "Enter the Private Subnet Ip Range of your Network[10.0.0.0/16]" IP_RANGE
 	if [[ -z $IP_RANGE ]]; then 
-	IP_RANGE="10.0.0.0/24"
+	IP_RANGE="10.0.0.0/16"
 	fi
 	read -p "Add WireGaurd Access True/False:  (y/n) [Y]" addwgaccess
 	if [[ -z $addwgaccess ]]; then
@@ -75,7 +75,7 @@ generate_grid_deployment_blocks() {
 	if [[ -z $VMNAME ]]; then
 		VMNAME=VM$i
 	fi
-    read -p "Choose a Description for VM $i[MyVm$i]: " VMDESC
+    read -p "Choose a Description for VM $i [MyVm$i]: " VMDESC
 	if [[ -z $VMDESC ]]; then
 		VMDESC=MyVm$i
 	fi
@@ -87,7 +87,7 @@ generate_grid_deployment_blocks() {
 	if [[ -z $NUM_CPU ]]; then
     NUM_CPU="4"
 	fi
-	read -p "Enter  [4096]: " NUM_MEMORY
+	read -p "Enter Amount of Memor in MB for Vm $i  [4096]: " NUM_MEMORY
 	if [[ -z $NUM_MEMORY ]]; then
     NUM_MEMORY="4096"
 	fi
@@ -148,7 +148,7 @@ generate_grid_deployment_blocks() {
 		MOUNT_POINT="/data$j"
 	fi
 
-    # Create disks block string with input values and add to array 
+    # Create disks block string with input values 
 	DISK_BLOCK="disks { \n	 name = \"$DISKNAME\" \n	 size = \"$DISKSIZE\" \n  } \n  "
 	
 	# Join DISK_BLOCKS array with newline separator
@@ -162,37 +162,127 @@ generate_grid_deployment_blocks() {
   done	
     
 	# Create vm block string with input values
-	VMS_BLOCK="resource \"grid_deployment\" \"D$i\" { \n  node = [$VMNODE] \n  network_name = \"$NetName\" \n  ${DISK_BLOCKS[*]}  vms { \n    name = \"$VMNAME\" \n    description = \"$VMDESC\" \n    flist = \"$FLIST\" \n    cpu = \"$NUM_CPU\" \n    publicip = \"$PUB4\" \n    publicip6 = \"$PUB6\" \n    memory = \"$NUM_MEMORY\" \n    ${MOUNT_BLOCKS[*]}planetary = \"$YGG\" \n    env_vars = { \n      SSH_KEY = \"$SSH_KEY\" \n    } \n  } \n} \n"
+	VMS_BLOCK="resource \"grid_deployment\" \"D$i\" { \n  node = $VMNODE \n  network_name = grid_network.net1.name \n  ${DISK_BLOCKS[*]}  vms { \n    name = \"$VMNAME\" \n    description = \"$VMDESC\" \n    flist = \"$FLIST\" \n    cpu = \"$NUM_CPU\" \n    publicip = \"$PUB4\" \n    publicip6 = \"$PUB6\" \n    memory = \"$NUM_MEMORY\" \n    ${MOUNT_BLOCKS[*]}planetary = \"$YGG\" \n    env_vars = { \n      SSH_KEY = \"\${var.SSH_KEY}\" \n    } \n  } \n} \n"
 
     
 	# Add vm block string to array
 	VMS_BLOCKS+=$(IFS=$'\n'; echo "${VMS_BLOCK[*]}")
 	
+	#  Call Function To Generate Output_blocks 
+	generate_vm_output_blocks
+	
 	
   done
 }
 
-# Funtion to check and create save path directory
-check_and_create_dir() {
-  # $1 is the directory to check
-  if [ ! -d "$1" ]; then
-    # Directory does not exist, so create it
-    mkdir "$1"
-  fi
+# Function to Generate Output_Blocks 
+generate_vm_output_blocks()  {
+ # Print list of available output options
+  echo "Available output variables:"
+  echo "1. output wiregaurd gateway configuration"
+  echo "2. output public IPV4 address of the VM "
+  echo "3. output public IPV6 address of the VM "
+  echo "4. output planetary network ip address of the VM"
+
+ # Request input for number of outputs to create for VM
+  read -p "Enter the number of output variables you want to create for VM $i[4]:" NUM_OUTPUTS
+  if [[ -z $NUM_OUTPUTS ]]; then
+		NUM_OUTPUTS="4"
+	fi
+  
+  # Initialize empty arrays to store disk and vm blocks
+  OUTPUT_BLOCKS=()
+  
+  # Loop through and request input for OUTPUT_NAME for each output
+  for (( j=1; j<=$NUM_OUTPUTS; j++ )); do
+    # Print list of available output options
+    echo "Enter the number of the output variable you want to create for VM $i (e.g. 1):"
+    read OUTPUT_NUM
+    # Get OUTPUT_NAME from available options
+    if [[ $OUTPUT_NUM -eq 1 ]]; then
+      # Create output block string with input values 
+	  OUTPUT_BLOCK+="output \"wg_config"$i"\" { \n  value = grid_network.net1.access_wg_config \n} \n"
+		# Join OUTPUT_BLOCKS array with newline separator
+	  OUTPUT_BLOCKS=$(IFS=$'\n'; echo "${OUTPUT_BLOCK[*]}")
+    elif [[ $OUTPUT_NUM -eq 2 ]]; then
+      # Create output block string with input values
+	  OUTPUT_BLOCK+="output \"public_ip"$i"\" {\n value = grid_deployment.D$i.vms[0].computedip \n} \n"
+	  # Join OUTPUT_BLOCKS array with newline separator
+	  OUTPUT_BLOCKS=$(IFS=$'\n'; echo "${OUTPUT_BLOCK[*]}")
+    elif [[ $OUTPUT_NUM -eq 3 ]]; then
+      OUTPUT_BLOCK+="output \"public_ip6"$i"\" { \n  value = grid_deployment.D$i.vms[0].computedip6 \n} \n"
+	  # Join OUTPUT_BLOCKS array with newline separator
+	  OUTPUT_BLOCKS=$(IFS=$'\n'; echo "${OUTPUT_BLOCK[*]}")
+    elif [[ $OUTPUT_NUM -eq 4 ]]; then
+      OUTPUT_BLOCK+="output \"ygg_ip"$i"\" { \n   value = grid_deployment.D$i.vms[0].ygg_ip \n} \n"
+	  # Join OUTPUT_BLOCKS array with newline separator
+	  OUTPUT_BLOCKS=$(IFS=$'\n'; echo "${OUTPUT_BLOCK[*]}")
+	fi
+  done
 }
 
-# Function to generate main.tf
-generate_main_tf() {
-  # Concatenate DISK_BLOCKS and VMS_BLOCKS arrays and create main.tf
-  printf "${NET_BLOCKS[*]}
-${VMS_BLOCKS[*]}" > $SAVE_PATH/main.tf
+ generate_main_tf() {
+  # Print variables block
+  printf "
+variable \"MNEMONICS\" {
+  type        = string
+  description = \"The mnemonic phrase used to generate the seed for the node.\"
+}
 
-  echo "main.tf successfully created at $SAVE_PATH/main.tf"
+variable \"NETWORK\" {
+  type        = string
+  default     = \"main\"
+  description = \"The network to connect the node to.\"
+}
+
+variable \"TF_PARALLELISM\" {
+  type        = number
+  default     = 1
+  description = \"The level of parallelism to use when applying changes.\"
+}
+
+variable \"SSH_KEY\" {
+  type = string
+}
+
+
+terraform {
+  required_providers {
+    grid = {
+      source = \"threefoldtech/grid\"
+    }
+  }
+}
+
+provider \"grid\" {
+    mnemonics = \"\${var.MNEMONICS}\"
+    network   = \"\${var.NETWORK}\"  
+}
+${NET_BLOCKS[*]}
+${VMS_BLOCKS[*]}
+${OUTPUT_BLOCKS[*]}" > $SAVE_PATH/main.tf
 }
 
 # Request path to save main.tf
 echo "Enter the path where you want to save main.tf (e.g. /home/user/):"
 read SAVE_PATH
+
+# Check and Create Directory 
+# check_and_create_dir "$save_path"
+
+# Prompt for identifier of grid_deployment resource
+# echo "Enter grid deployment identifier:"
+# read GRID_ID
+
+# Request node IDs for the grid_deployment resource
+#read -p "Enter the node IDs of all the nodes you would like to deploy on[1, 2]:" GRID_NODE
+#if [[ -z $GRID_NODE ]]; then
+#  GRID_NODE="1"
+ # fi
+
+# Request network name
+#echo "Enter the name of the grid network:"
+#read  GRID_NETNAME
 
 # Call generate_grid_network_blocks
 generate_grid_network_blocks
